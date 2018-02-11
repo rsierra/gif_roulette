9897f84c58a05a23595186933be73e0930dad32f
Ruben Sierra <maguilag@gmail.com>
2018-02-12 00:23:28 +0100

    Store the channel in the roulette object

--- assets/js/roulette.js
+++ assets/js/roulette.js
@@ -1,22 +1,23 @@
 import {Presence} from "phoenix"
 
 let Roulette = {
+  channel: null,
   init(socket) {
-    let channel = socket.channel("roulette:lobby", {})
-    channel.join()
       .receive("ok", resp => { console.log("Joined successfully", resp) })
+    Roulette.channel = socket.channel("roulette:lobby", {})
+    Roulette.channel.join()
       .receive("error", resp => { console.log("Unable to join", resp) })
-    channel.onError(e => console.log("Something went wrong", e))
-    channel.onClose(e => console.log("Channel closed", e))
+    Roulette.channel.onError(e => console.log("Something went wrong", e))
+    Roulette.channel.onClose(e => console.log("Channel closed", e))
 
     let presences = {}
 
-    channel.on("presence_state", state => {
+    Roulette.channel.on("presence_state", state => {
       presences = Presence.syncState(presences, state);
       document.querySelector("#users-count").innerText = Object.keys(presences).length;
     })
 
-    channel.on("presence_diff", diff => {
+    Roulette.channel.on("presence_diff", diff => {
       presences = Presence.syncDiff(presences, diff)
       document.querySelector("#users-count").innerText = Object.keys(presences).length;
     })
