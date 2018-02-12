413fa26589795f78408357e041959ba20e9cd608
Ruben Sierra <maguilag@gmail.com>
2018-02-12 01:21:12 +0100

    Add giphy api lib

--- config/dev.exs
+++ config/dev.exs
@@ -47,3 +47,7 @@ config :logger, :console, format: "[$level] $message\n"
 # Set a higher stacktrace during development. Avoid configuring such
 # in production as building large stacktraces may be expensive.
 config :phoenix, :stacktrace_depth, 20
+
+# Finally import the config/prod.secret.exs
+# which should be versioned separately.
+import_config "dev.secret.exs"
